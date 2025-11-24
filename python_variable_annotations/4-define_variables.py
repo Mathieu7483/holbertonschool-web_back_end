f07fd20ef7d3806963ab10ab2b50ecd53cd3dd86
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 10:02:26 +0100

    Remove unnecessary comment about global variable annotations

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -4,7 +4,7 @@ This module defines variables with specific types using variable annotations.
 """
 import math
 
-# Global variable annotations (Note the spaces around '=' for assignment)
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
