7564290b7aa748522ee65dcc28fbb4b9a7dd125e
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 14:59:51 +0100

    Add 3-tasks module to create asynchronous tasks for wait_random coroutines

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""3-tasks module
+This module creates asynchronous tasks for multiple wait_random coroutines.
+"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Creates an asyncio Task that runs wait_random with the specified max_delay.
+    Args:
+        max_delay (int): Maximum delay value for wait_random.
+    Returns:
+        asyncio.Task: An asyncio Task running wait_random.
+    """
+    coroutine = wait_random(max_delay)
+    task = asyncio.create_task(coroutine)
+
+    return task
