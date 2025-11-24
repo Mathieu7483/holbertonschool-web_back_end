9471fd6ea1490a3bb4d376ee5852929f5721cce3
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 13:16:48 +0100

    Add zoom_array function to repeat elements in a tuple

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""102-type_checking module
+This module contains a function that zooms into a tuple by
+repeating each element a specified number of times.
+"""
+from typing import List, Tuple
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Zooms into a tuple by repeating each element.
+
+    Args:
+        lst (Tuple): A tuple of elements to be zoomed.
+        factor (int, optional): The number of times to repeat each element.
+                                Defaults to 2.
+
+    Returns:
+        List: A new list with each element repeated 'factor' times.
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
