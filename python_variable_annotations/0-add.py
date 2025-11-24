97f8f5b5e03e79da94abd4852e0aae854a952ca2
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 09:42:25 +0100

    Refactor add function to accept only float inputs and simplify return statement

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,17 +2,16 @@
 """0-add module
 This module contains a function that adds two integers.
 """
-from typing import Union
 
 
-def add(a: Union[int, float], b: Union[int, float]) -> float:
+def add(a:float, b:float) -> float:
     """Adds two numbers and returns the result as a float.
 
     Args:
-        a (Union[int, float]): The first number.
-        b (Union[int, float]): The second number.
+        a (float]): The first number.
+        b (float): The second number.
 
     Returns:
         float: The sum of a and b.
     """
-    return float(a + b)
+    return a + b
