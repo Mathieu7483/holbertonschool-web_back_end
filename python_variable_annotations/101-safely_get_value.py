328e9b9f3a537794fbd76c2b4762aca8c36f596e
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 11:17:02 +0100

    Update return type of safely_get_value function to Union[Any, T]

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -12,7 +12,7 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping[Any, T],
                      key: Any,
-                     default: Optional[T] = None) -> Union[T, Optional[T]]:
+                     default: Optional[T] = None) -> Union[Any, T]:
     """Safely retrieves a value from a dictionary by key.
 
     Args:
