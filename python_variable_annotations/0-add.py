e651a2671bc69192c0494e1500946114658661cc
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 08:47:22 +0100

    Add 0-add module with function to add two integers

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""0-add module
+This module contains a function that adds two integers.
+"""
+
+
+def add(a: int, b: int) -> int:
+    """Adds two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int): The second integer.
+
+    Returns:
+        int: The sum of the two integers.
+    """
+    return a + b
