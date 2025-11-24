873b056bd208314350cf16a18714105d7e62e92a
Mathieu7483 <11436@holbertonstudents.com>
2025-11-24 09:46:29 +0100

    Update module documentation to clarify that the add function operates on floats

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """0-add module
-This module contains a function that adds two integers.
+This module contains a function that adds two floats.
 """
 
 
-def add(a:float, b:float) -> float:
-    """Adds two numbers and returns the result as a float.
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns the result as a float.
 
     Args:
-        a (float]): The first number.
-        b (float): The second number.
+        a (float): The first float number.
+        b (float): The second float number.
 
     Returns:
         float: The sum of a and b.
