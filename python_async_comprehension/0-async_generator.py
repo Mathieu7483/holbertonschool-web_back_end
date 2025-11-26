80a41dd89c2e79603a7e69df93d3f2ff7e8533d1
Mathieu7483 <11436@holbertonstudents.com>
2025-11-26 10:11:57 +0100

    Fix async_generator return type and correct random number generation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ random numbers with a delay.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronous generator that yields 10 random numbers
     between 0 and 10, each after a random delay between 0 and 10 seconds.
 
@@ -16,6 +16,6 @@ async def async_generator() -> AsyncGenerator[float, None, None]:
         float: A random float number between 0 and 10.
     """
     for _ in range(10):
-        delay = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield delay
+        random_number = random(0, 10)
+        yield random_number
