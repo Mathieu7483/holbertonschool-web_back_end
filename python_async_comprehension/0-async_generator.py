eb671da5b713100742d5f458ac98ba5caae48588
Mathieu7483 <11436@holbertonstudents.com>
2025-11-26 10:10:16 +0100

    Fix async_generator type hint and correct sleep duration to 1 second

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Asynchronous generator that yields 10 random numbers
     between 0 and 10, each after a random delay between 0 and 10 seconds.
 
@@ -17,5 +17,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         delay = random.uniform(0, 10)
-        await asyncio.sleep(delay)
+        await asyncio.sleep(1)
         yield delay
