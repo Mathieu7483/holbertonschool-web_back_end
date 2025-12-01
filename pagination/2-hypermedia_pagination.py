a4dba98f089c6c23a4847497d4bae337a8ffa067
Mathieu7483 <11436@holbertonstudents.com>
2025-12-01 10:28:57 +0100

    Fix formatting of docstring in dataset method of Server class

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -31,8 +31,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
