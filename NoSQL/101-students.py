28447d8ee168312210833b68aa04ec1650257ed9
Mathieu7483 <11436@holbertonstudents.com>
2025-12-01 14:53:20 +0100

    Add function to return all students sorted by average score

---
+++ NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""function that returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    pipeline = [
+        {
+            "$unwind": "$topics"
+        },
+        {
+            "$group": {
+                "_id": "$_id",
+                "name": {"$first": "$name"},
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    result = list(mongo_collection.aggregate(pipeline))
+    return result
