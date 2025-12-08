94f95dde53a9d137e5f763c78c3738f3bf0ced7e
Mathieu7483 <mathieu.godalier@orange.fr>
2025-12-08 11:51:07 +0100

    Fix loop iteration in appendToEachArrayValue to use array.keys() for correct indexing

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
+  for (const idx of array.keys()) {
     let value = array[idx];
     array[idx] = appendString + value;
   }
