147cb0c4be18ba989fbb0a56fc2178332c52c3ab
Mathieu7483 <mathieu.godalier@orange.fr>
2025-12-08 13:21:53 +0100

    Refactor createIteratorObject to simplify employee iteration logic

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,33 +1,6 @@
 export default function createIteratorObject(report) {
-return {
-  [Symbol.iterator]() {
-    const departments = Object.keys(report.allEmployees);
-    let deptIndex = 0;
-    let empIndex = 0;
-
-    return {
-      next() {
-        if (deptIndex >= departments.length) {
-          return { done: true };
-        }
-
-        const departmentName = departments[deptIndex];
-        const employees = report.allEmployees[departmentName];
-
-        if (empIndex >= employees.length) {
-          deptIndex++;
-          empIndex = 0;
-          return this.next();
-        }
-
-        const value = {
-          department: departmentName,
-          employee: employees[empIndex],
-        };
-        empIndex++;
-        return { value, done: false };
-      },
-    };
-  },
-};
+const allEmployees = [];
+for (const department of Object.values(report.allEmployees)) {
+  allEmployees.push(...department);}
+return allEmployees[Symbol.iterator]();
 }
