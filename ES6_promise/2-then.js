deaaa0fd17ef1eaa216a4019f23e17d8b4e3506d
Mathieu7483 <mathieu.godalier@orange.fr>
2025-12-15 13:31:23 +0100

    Refactor handleResponseFromAPI to return response object and improve error handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
-      console.log("Got a response from the API");
+      return {
+        status: 200,
+        body: 'success'
+      };
     })
     .catch(() => {
-      console.log("Error getting response from the API");
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
