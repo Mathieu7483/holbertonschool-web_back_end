67f6ab7efba0b24f557dec172e2d5a3b203644a1
Mathieu7483 <mathieu.godalier@orange.fr>
2025-12-15 14:45:29 +0100

    Refactor handleProfileSignup to use Promise.allSettled for improved error handling

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,37 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.all([userPromise, photoPromise.catch(err => err)])
-    .then(results => {
-      const photoResult = results[1];
-      let photoStatus, photoValue;
-
-      const userResult = {
-        status: 'fulfilled',
-        value: results[0]
-      };
-
-      if (typeof photoResult === 'string' && photoResult.includes('cannot be processed')) {
-          photoStatus = 'rejected';
-          photoValue = `Error: ${photoResult}`; 
-      } else {
-          photoStatus = 'fulfilled';
-          photoValue = photoResult;
-      }
-      
-      return [
-        userResult,
-        {
-          status: photoStatus,
-          value: photoValue
-        }
-      ];
-    })
-    .catch(error => {
-      console.error('Unexpected error:', error);
-      return [];
-    });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(result =>
+      result.map(r => r.status === 'fulfilled' ? r : { status: r.status, value: r.reason.toString() })
+    );
 }
