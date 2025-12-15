715b63af4880ce08e933ddc26aa0c46c38361242
Mathieu7483 <mathieu.godalier@orange.fr>
2025-12-15 14:42:12 +0100

    Refactor handleProfileSignup to use Promise.all for user and photo upload handling

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,34 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise]);
+  return Promise.all([userPromise, photoPromise.catch(err => err)])
+    .then(results => {
+      const photoResult = results[1];
+      let photoStatus, photoValue;
+
+      const userResult = {
+        status: 'fulfilled',
+        value: results[0]
+      };
+
+      if (typeof photoResult === 'string' && photoResult.includes('cannot be processed')) {
+          photoStatus = 'rejected';
+          photoValue = `Error: ${photoResult}`; 
+      } else {
+          photoStatus = 'fulfilled';
+          photoValue = photoResult;
+      }
+      
+      return [
+        userResult,
+        {
+          status: photoStatus,
+          value: photoValue
+        }
+      ];
+    })
+    .catch(error => {
+      console.error('Unexpected error:', error);
+      return [];
+    });
 }
