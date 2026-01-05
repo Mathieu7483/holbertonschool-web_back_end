f85ab79f188cb2b0a6ce4524fb638b4d39fc7901
Mathieu7483 <mathieu.godalier@orange.fr>
2026-01-05 13:02:26 +0100

    Refactor HTTP server implementation for clarity and consistency

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
-http=require("http")
+const http = require('http');
 
-app=http.createServer()
-app.on("request",(req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'})
-    res.end("Hello Holberton School!")
-})
-app.listen(1245, ()=>{
-    console.log("Server is listening on port 1245")
-})
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+module.exports = app;
