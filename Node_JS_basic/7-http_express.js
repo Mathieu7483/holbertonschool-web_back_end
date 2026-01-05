1504d9c087848173ee030ea1be43f79686035e59
Mathieu7483 <mathieu.godalier@orange.fr>
2026-01-05 13:53:18 +0100

    Add Express HTTP server implementation in 7-http_express.js

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = process.argv[2];
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const responseParts = ['This is the list of our students'];
+  const originalLog = console.log;
+
+  console.log = (msg) => responseParts.push(msg);
+
+  try {
+    await countStudents(DATABASE);
+    console.log = originalLog;
+    res.status(200).send(responseParts.join('\n'));
+  } catch (error) {
+    console.log = originalLog;
+    res.status(500).send(`${responseParts[0]}\n${error.msg}`);
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(1245);
+
+module.exports = app;
