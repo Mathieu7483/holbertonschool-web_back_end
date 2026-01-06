bc3dc390826fc6194b5be4de7c08a120b98619d2
Mathieu7483 <mathieu.godalier@orange.fr>
2026-01-06 08:38:23 +0100

    Add utility function to read database and organize students by field

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const studentsByField = {};
+    
+    const rows = lines.slice(1);
+
+    for (const row of rows) {
+      const [firstname, , , field] = row.split(',');
+      if (!studentsByField[field]) studentsByField[field] = [];
+      studentsByField[field].push(firstname);
+    }
+    resolve(studentsByField);
+  });
+});
+
+export default readDatabase;
