72b1a409298ddc7c2118c9f18742c3aae69df740
Mathieu7483 <mathieu.godalier@orange.fr>
2026-01-15 15:49:48 +0100

    Refactor asyncUploadUser function for simplified response handling

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -2,31 +2,18 @@ import uploadPhoto from './utils.js';
 import createUser from './utils.js';
 
 export default async function asyncUploadUser() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
-  const results = await Promise.allSettled([photoPromise, userPromise]);
-  
-  const response = {
-    photo: null,
-    user: null,
-  };
-  results.forEach((result) => {
-    if (result.status === 'fulfilled') {
-      if (result.value && result.value.id && result.value.name) {
-        response.user = result.value;
-      } else {
-        response.photo = result.value;
-      }
-    } else {
-      if (result.reason && result.reason instanceof Error) {
-        if (result.reason.message.includes('Photo')) {
-          response.photo = { error: result.reason.message };
-        } else if (result.reason.message.includes('User')) {
-          response.user = { error: result.reason.message };
-        }
-      }
-    }
-  });
-  
-  return response;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
